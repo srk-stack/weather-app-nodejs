04418cd65fc374889f56f794542fb43a2ccb3f5e
Kolli, SatyaRohit <satyarohit.kolli@monsanto.com>
2020-06-16 18:13:01 +0530

    secret keys changed for APIs

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,14 +1,17 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoicm9oaXRjb2RlcyIsImEiOiJjazduYmNxM2cwMTRsM2ZtdDJnbDd4OXVqIn0.rC8YAJb2uYx_BG_XYQqHFQ&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (body.features.length === 0) {
-            callback('Unable to find location. Try another search.', undefined)
+        } else if (body.features) {
+            if (body.features.length === 0) {
+                callback('Unable to find location. Try another search.', undefined)
+            }
         } else {
+            console.log('body', body)
             callback(undefined, {
                 latitude: body.features[0].center[1],
                 longitude: body.features[0].center[0],
