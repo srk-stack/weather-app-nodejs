cb55a650ca287724ad2c876416c444496c8c917e
Kolli, SatyaRohit <satyarohit.kolli@monsanto.com>
2020-06-17 21:51:44 +0530

    shows temperature in celsius, adds text to about page.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            let temperature = (body.currently.temperature - 32) * (5 / 9)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + temperature.toFixed(2) + '°C outside. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
